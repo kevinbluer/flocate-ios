3386a9b5d47d69976a3553409443f3273f922744
Kevin Bluer <kevin@bluer.com>
2014-10-01 10:24:11 +0800

    Zooming in on the map in the Location Detail view

--- Flocate/LocationDetailView.swift
+++ Flocate/LocationDetailView.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationDetailViewController: UIViewController  {
     @IBOutlet weak var labelLocationName: UILabel!
-    var entry:AnyObject = "x"
+    var entry:AnyObject = ""
     @IBOutlet weak var labelLocationWhat: UILabel!
     @IBOutlet weak var buttonDone: UIButton!
+    @IBOutlet weak var mapLocation: MKMapView!
     
     override func viewDidLoad() {
         labelLocationName.text = entry["Note"] as String!
@@ -24,6 +26,25 @@ class LocationDetailViewController: UIViewController  {
         buttonDone.layer.cornerRadius = 5
         buttonDone.layer.borderWidth = 1
         buttonDone.layer.borderColor = UIColor( red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0 ).CGColor
+        
+        var geopoint:PFGeoPoint = entry["Location"] as PFGeoPoint
+        
+        var location = CLLocationCoordinate2D(
+            latitude: geopoint.latitude,
+            longitude: geopoint.longitude
+        )
+        
+        // add new annotation
+        var pinLocation = MKPointAnnotation()
+        pinLocation.coordinate = location
+        pinLocation.title = (entry["Doing"] as String)
+        self.mapLocation.addAnnotation(pinLocation)
+        
+        var span = MKCoordinateSpanMake(0.01, 0.01)
+        var region = MKCoordinateRegion(center: location, span: span)
+        self.mapLocation.setRegion(region, animated: true)
+        self.mapLocation.selectAnnotation(pinLocation, animated: true)
+        
     }
     
 }
